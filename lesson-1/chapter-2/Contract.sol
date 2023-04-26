c8f4a7c197a91a24f3f4efa4f0353298144620e5
LucaPiccirillo <67259116+LucaPiccirillo@users.noreply.github.com>
2023-04-26 16:12:12 +0200

    Update Contract.sol

--- lesson-1/chapter-2/Contract.sol
+++ lesson-1/chapter-2/Contract.sol
@@ -1,16 +1,13 @@
-
 pragma solidity ^0.4.25;
 
-      contract ZombieFactory {
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 
-          uint dnaDigits = 16;
-          uint dnaModulus = 10 ** dnaDigits;
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 
-          struct Zombie {
-              string name;
-              uint dna;
-          }
-
-          Zombie[] public zombies;
-
-      }
+    Zombie[] public zombies;
+}
